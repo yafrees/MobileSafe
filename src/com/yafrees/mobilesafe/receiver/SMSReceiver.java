e9ef2d5e54e49e7dd7b450537df6f3b7775ad142
yafrees <yafree18@gmail.com>
2016-05-10 23:33:40 +0800

    播放报警音乐。

--- src/com/yafrees/mobilesafe/receiver/SMSReceiver.java
+++ src/com/yafrees/mobilesafe/receiver/SMSReceiver.java
@@ -1,10 +1,13 @@
 package com.yafrees.mobilesafe.receiver;
 
+import com.yafrees.mobilesafe.R;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.media.MediaPlayer;
 import android.telephony.SmsMessage;
 
 /**
@@ -14,22 +17,22 @@ import android.telephony.SmsMessage;
 public class SMSReceiver extends BroadcastReceiver {
 
 	SharedPreferences sp;
-	
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		sp = context.getSharedPreferences("config",Context.MODE_PRIVATE);
-		
+
 		Object [] pdus = (Object[]) intent.getExtras().get("pdus");
 		for (Object pdu : pdus) {
 			SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdu);
-			
+
 			//得到发送者
 			String sender = sms.getOriginatingAddress();
 			String safenumber = sp.getString("safenumber", "");
-			
+
 			//得到短信内容
 			String body = sms.getMessageBody();
-			
+
 			if (sender.contains(safenumber)) {
 				if ("#location#".equals(body)) {
 					//得到手机的Gps位置
@@ -38,10 +41,14 @@ public class SMSReceiver extends BroadcastReceiver {
 				}
 				else if ("#alarm#".equals(body)) {
 					//播放报警音乐
+					MediaPlayer player = MediaPlayer.create(context, R.raw.the_sun_also_rise);
+					player.setVolume(1.0f, 1.0f);
+					player.setLooping(true);
+					player.start();
 					System.out.println("播放报警音乐");
 					abortBroadcast();
 				}
-				else if ("#wipedate#".equals(body)) {
+				else if ("#wipedata#".equals(body)) {
 					//远程删除数据
 					System.out.println("远程删除数据");
 					abortBroadcast();
