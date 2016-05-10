66dd968bd8d87973636a6a5550231fc234f47362
yafrees <yafree18@gmail.com>
2016-05-10 20:58:01 +0800

    SIM卡变更后，向预先设置的安全号码发送短信。

--- src/com/yafrees/mobilesafe/receiver/BootCompleteReceiver.java
+++ src/com/yafrees/mobilesafe/receiver/BootCompleteReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Message;
+import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
@@ -22,22 +24,27 @@ public class BootCompleteReceiver extends BroadcastReceiver{
 	public void onReceive(Context context, Intent intent) {
 		//1.得到之前的sim卡信息
 		sp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
-		String save_sim = sp.getString("sim", "");
+		if (sp.getBoolean("protect", false)) {
 
-		//2.得到当前手机的sim卡信息
-		tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-		String currentSIM = tm.getSimSerialNumber();
+			String save_sim = sp.getString("sim", "");
 
-		//3.比较sim卡信息是否一致
-		if (save_sim.equals(currentSIM)) {
-			//如果一致就什么都不用做
+			//2.得到当前手机的sim卡信息
+			tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+			String currentSIM = tm.getSimSerialNumber() + "yafree";
+
+			//3.比较sim卡信息是否一致
+			if (save_sim.equals(currentSIM)) {
+				//如果一致就什么都不用做
+			}
+			else {
+				//4.如果不一致，就发送信息给安全号码
+				System.out.println("SIM卡变更了...");
+				Toast.makeText(context, "SIM变更...", 1).show();
+
+				SmsManager.getDefault()
+				.sendTextMessage(sp.getString("safenumber", ""), null, 
+						"SIMCard changed from yafrees...", null, null);
+			}
 		}
-		else {
-			//如果不一致，就发送信息给安全号码
-			Toast.makeText(context, "SIM变更...", 1).show();
-		}
-
-		//4.如果不一致就发短信给安全号码
 	}
-
 }
